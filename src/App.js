38bbafcd3282d5af1a8ed75fdf5aeefd1b6700c4
Harsh Sharma <harshsharma20503@gmail.com>
2023-01-11 12:44:37 +0530

    login/singup and header functionality added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './Components/Header.js'
 import {useState, useEffect} from 'react';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route, useNavigate} from "react-router-dom";
 import Home from './Pages/Home';
 import Detail from './Pages/Detail';
 import AddEditBlog from './Pages/AddEditBlog';
@@ -11,29 +11,58 @@ import About from './Pages/About';
 import NotFound from './Pages/NotFound';
 import Auth from './Pages/Auth';
 import Events from './Pages/Events';
+import { auth } from './firebase';
+import { signOut } from 'firebase/auth';
 
 
 function App() {
+
+  const [active,setActive] = useState("home");
+  const [user,setUser] = useState(null);
+  
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        setUser(authUser);
+      }else{
+        setUser(null);
+      }
+    });
+  },[]);
+
+  const handleLogout = () =>{
+    signOut(auth).then(()=>{
+      setUser(null);
+      setActive("login");
+      navigate("/auth");
+    })
+  }
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header/> 
-        <ToastContainer position='top-center'/>
-        <div className="middleSection">
-          <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/detail/:id' element={<Detail/>}/>
-          <Route path='/create' element={<AddEditBlog/>}/>
-          <Route path='/update/:id' element={<AddEditBlog/>} />
-          <Route path='/about' element={<About/>}/>
-          <Route path='/events' element={<Events/>}/>
-          <Route path='/auth' element={<Auth />}/>
-          <Route path='*' element={<NotFound />}/>
-          </Routes>
-        </div>
+    <div className="App">
+      <Header 
+      setActive={setActive} 
+      active={active} 
+      user={user}
+      handleLogout={handleLogout}
+      />
+      <ToastContainer position='top-center'/>
+      <div className='middleSection'>
+      <Routes>
+        <Route path='/' element={<Home setActive={setActive} user={user}/>}/>
+        <Route path='/detail/:id' element={<Detail setActive={setActive}/>}/>
+        <Route path='/create' element={<AddEditBlog  user={user} />}/>
+        <Route path='/update/:id' element={<AddEditBlog user={user} setActive={setActive}/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/events' element={<Events/>}/>
+        <Route path='/auth' element={<Auth setActive={setActive} setUser={setUser}/>}/>
+        <Route path='*' element={<NotFound setActive={setActive}/>}/>
+      </Routes>
       </div>
-    </BrowserRouter>
-  );
+    </div>
+);
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 
 import './Components.scss'
 
-const Navbar = () => {
-  
-
+const Header = ({active,setActive,user,handleLogout}) => {
+  const userId=user?.uid;
   return (
     <>
     <section className="header">
@@ -18,40 +17,54 @@ const Navbar = () => {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link"  to="/" >Home</Link>
+                  <Link className={`nav-link ${active==="home"?"active":""}`} to="/" onClick={()=> setActive("home")}>Home</Link>
                 </li>
+                {userId ? (
+                  <>    
                     <li className="nav-item">
-                      <Link className= "nav-link" to="/create">Create</Link>
+                      <Link className={`nav-link ${active==="create"?"active":""}`} to="/create" onClick={()=> setActive("create")}>Create</Link>
                     </li>
 
                     <li className="nav-item">
-                      <Link className="nav-link" to="/events">Events</Link>
+                      <Link className={`nav-link ${active==="events"?"active":""}`} to="/events" onClick={()=> setActive("events")}>Events</Link>
                     </li>
 
                     <li className="nav-item">
-                      <Link className='nav-link' to="/" rel="noreferrer" target="_blank">Resources</Link>
-                    </li>                
+                      <a className='nav-link' href="https://drive.google.com/drive/folders/1O_jJ2SlRIBRB8eReNYuKfyVV7YnQH9Z0?usp=sharing" rel="noreferrer" target="_blank">Resources</a>
+                    </li>
+                  </> 
+                ) : (
+                  <>
+                  </>
+                )}
                 <li className="nav-item">
-                  <Link className="nav-link" to="/about">About</Link>
+                  <Link className={`nav-link ${active==="about"?"active":""}`} to="/about" onClick={()=> setActive("about")}>About</Link>
                 </li>
+                {userId ? (
+                  <>
                     <li className='nav-item profileName'>
                       <Dropdown className='nav-link' style={{padding:"0px"}}>
                         <Dropdown.Toggle variant="success" id="dropdown-basic" className="color">
                           <img className='profileImg' src="https://e7.pngegg.com/pngimages/423/895/png-clipart-computer-icons-user-male-user-miscellaneous-head.png" alt=""
                           />
-                              Dipti
+                          {user?.displayName}
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                          <Dropdown.Item >Logout</Dropdown.Item>
+                          <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                         </Dropdown.Menu>
                       </Dropdown>
                     </li>
+                  </>
+                ) : (
                   <li className="nav-item">
-                    <Link className="nav-link" to="/auth" >Login</Link>
+                    <Link className={`nav-link ${active==="login"?"active":""}`} to="/auth" onClick={()=> setActive("login")}>Login</Link>
                   </li>
+                )}
+
               </ul>
             </div>
           </div>
@@ -64,4 +77,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Header
